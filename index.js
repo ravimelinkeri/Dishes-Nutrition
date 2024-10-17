f32f8e323d17fe880649c70d45db4d16498e3566
ravimelinkeri <ravimelinkeribs@gmail.com>
2024-10-17 10:30:18 +0530

    added search input field

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-const recipeContainer = document.querySelector('.recipes-container')
+const recipeContainer = document.querySelector(".recipes-container");
 
-let Alldishesrecipe
+let Alldishesrecipe;
+
+const Searchbar = document.querySelector('.search-icon-input input')
+
+fetch("https://ravimelinkeri.github.io/dishes_json/dishes.json")
+  .then((res) => res.json())
+  .then((data) => {
+    randerdata(data);
+    Alldishesrecipe = data;
+  });
+
+Searchbar.addEventListener('input', (e)=>{
+  // console.log(e.target.value);
+  // console.log(Alldishesrecipe);
+  
+  const filteredDish = Alldishesrecipe.filter((Dish)=> Dish.name.Dish.toLowerCase().includes(e.target.value.toLowerCase()))
+  randerdata(filteredDish);
+})
 
 
-fetch('https://ravimelinkeri.github.io/dishes_json/dishes.json')
-.then((res) => res.json())
-.then(data => {
+function randerdata(data) {
+  recipeContainer.innerHTML = "";
 
-    randerdata(data)
-    Alldishesrecipe = data
-}
-   )
+  data.forEach((Dish) => {
+    //  console.log(Dish.name.Dishname);
 
-function randerdata(){
-    recipeContainer.innerHTML= ''
+    const recipeCard = document.createElement("a");
+    recipeCard.classList.add("recipe-card");
+    recipeCard.href = `recipe-details.html?name=${Dish.name.Dishname}`;
 
-    data.forEach((Dish) =>{
- 
-        //  console.log(Dish.name.Dishname);
-    
-    
-        
-        const recipeCard = document.createElement('a')
-        recipeCard.classList.add('recipe-card')
-        recipeCard.href = `recipe-details.html?name=${Dish.name.Dishname}`
-    
-        recipeCard.innerHTML = `
+    recipeCard.innerHTML = `
          <img src="${Dish.name.img}" alt="img">
                 <div class="small-intro">
                     <h3 class="title"> ${Dish.name.Dish} </h3>
@@ -36,15 +41,8 @@ function randerdata(){
                     <h3 class="Nutrition-del"> Preparation Time</h3>
                     <p><span class="bold"> Time for Cook : </span>${Dish.name.totalTime} </p>
                 </div>
-    `
-    
-    
-    recipeContainer.append(recipeCard)
-         
-     
-    })
+    `;
 
+    recipeContainer.append(recipeCard);
+  });
 }
-
-
-
